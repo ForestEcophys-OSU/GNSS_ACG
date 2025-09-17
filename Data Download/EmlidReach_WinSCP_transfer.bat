6481f5f294120ac385005ddf1f3591ca0922f948
German Vargas G. <vargasgg@oregonstate.edu>
2025-09-17 15:55:39 -0700

    English annotations of the WinSCP transfer file

---
+++ Data Download/EmlidReach_WinSCP_transfer.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+:: Date 
+for /f %%i in ('powershell -command "Get-Date -Format yyyy-MM-dd"') do set DATE=%%i
+
+:: Detect hotspot name
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interfaces ^| findstr /C:"SSID" ^| findstr /V "BSSID"') do set REACH=%%a
+set REACH=%REACH:~1%
+
+:: Output directory
+set outDir=C:\Users\username\Documents\GNSS_PROJECT\%REACH%\%DATE%
+if not exist "%outDir%" mkdir "%outDir%"
+
+:: Temp script for WinSCP
+set WINSCP_SCRIPT=%TEMP%\script_winscp.txt
+echo open sftp://reach:emlidreach@192.168.42.1 > "%WINSCP_SCRIPT%"
+echo lcd "%outDir%" >> "%WINSCP_SCRIPT%"
+echo cd /data/logs/ >> "%WINSCP_SCRIPT%"
+echo get *.* >> "%WINSCP_SCRIPT%"
+echo exit >> "%WINSCP_SCRIPT%"
+
+:: Ejecutar transferencia
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /script="%WINSCP_SCRIPT%"
+
+echo ===== Transferencia finalizada =====
+echo File saved in: %outDir%
+pause
